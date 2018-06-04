9f3f9bcafc88a7be712af7ee2f6796555ad56243
佐藤大介 <workshift56@gmail.com>
2018-06-04 23:41:01 +0900

    googleBooksAPIの情報を取得

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,19 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
 class HomeController < ApplicationController
   def index
   end
 
   def search
-    @keyword = params[:keyword]
+    keyword = params[:keyword]
+
+    url = URI.parse(URI.escape("https://www.googleapis.com/books/v1/volumes?q=#{keyword}"))
+    res = Net::HTTP.start(url.host, url.port, use_ssl: true){|http|
+      http.get(url.path + "?" + url.query);
+    }
+    obj = JSON.parse(res.body)
+    binding.pry
   end
 end
